a33ee1a09bd0e6fc00d741c2ff0ec6dff0345dc1
andyleeking9191 <andyleeking9191@users.noreply.github.com>
2020-07-27 21:37:03 -0400

    Created a helper function to avoid code repetition

--- scripts.js
+++ scripts.js
@@ -7,30 +7,38 @@ const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&coun
 
 let photosArray = [];
 
-function displayPhotos() {
-    photosArray.forEach((photo) => {
-        const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
-        const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
-        item.appendChild(img);
-        imageContainer.appendChild(item);
-    });
+function setAttributes(element, obj) {
+  for (const key in obj) {
+    element.setAttribute(key, obj[key]);
+  }
 }
 
+function displayPhotos() {
+  photosArray.forEach((photo) => {
+    const item = document.createElement('a');
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    });
+    const img = document.createElement('img');
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
 
 async function getApiPhotos() {
-    try {
-        const response = await fetch(apiUrl);
-        photosArray = await response.json();
-        displayPhotos();
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-getApiPhotos()
-
+getApiPhotos();
